fce8488128fd41fec0f50534e18541f0d14f85d8
Shah-yan <118072669+Shah-yan@users.noreply.github.com>
2025-11-13 22:48:03 +0530

    Added my 1st file to this repo.

---
+++ 00_day1_1day.py
@@ -0,0 +1,10 @@
+# This is the start of new era and the whole new effort In-Sha-ah.
+
+print("Your are not in burden of things you have.")
+print("You are chosen for the purpose you eyes can't see.")
+print("Lazyness of 25 years, Can be covered by the hardwork of 1 year.")
+print("Give yourself time to heal, Not an excuse to start from tomorrow....!")
+
+
+# You can run the python code by the play button of computer.
+# Also you can write >> python <file_name> << in the terminal from the directory where the file is present. 
